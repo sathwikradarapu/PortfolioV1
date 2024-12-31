f0ff2248fbc2cff74616483d5481fdc42ec77f5b
sathwik <sathwikradarapu@gmail.com>
2024-12-31 18:42:37 +0530

    second commit

--- src/components/TestEx.jsx
+++ src/components/TestEx.jsx
@@ -5,10 +5,10 @@ const TestEx = () => {
     <div className="flex items-center justify-center">
       <div className="flex flex-wrap items-center justify-center gap-2 m-4">
         <ul className="flex flex-wrap footerIcons">
-        <li>
-        <a href="mailto:sathwikradarapu@gmail.com?subject=Your Subject Here&body=Your message here.">
-          <i class="fas fa-envelope icon"></i>
-        </a>
+          <li>
+            <a href="mailto:sathwikradarapu@gmail.com?subject=Your Subject Here&body=Your message here.">
+              <i class="fas fa-envelope icon"></i>
+            </a>
           </li>
           <li>
             <a href="https://www.linkedin.com/in/sathwik-radarapu-b9111a273/">
@@ -17,12 +17,12 @@ const TestEx = () => {
           </li>
           <li>
             <a href="https://github.com/sathwikradarapu">
-            <i class="fab fa-github icon"></i>
+              <i class="fab fa-github icon"></i>
             </a>
           </li>
           <li>
-            <a href="https://leetcode.com/u/Sathwik_Radarapu2933/">
-            <i class="fas fa-code icon"></i>
+            <a href="https://leetcode.com/u/Sathwik_Radarapu/">
+              <i class="fas fa-code icon"></i>
             </a>
           </li>
         </ul>
